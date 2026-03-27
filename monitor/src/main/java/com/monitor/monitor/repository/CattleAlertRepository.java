66c866748113b1fc75fb4e9d8d0f30ae626d28cb
Kaarthik-S12 <kaarthik.s2023ece@sece.ac.in>
2026-03-27 15:44:39 +0530

    f

---
+++ monitor/src/main/java/com/monitor/monitor/repository/CattleAlertRepository.java
@@ -0,0 +1,9 @@
+package com.monitor.monitor.repository;
+
+import com.monitor.monitor.model.CattleAlert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CattleAlertRepository extends JpaRepository<CattleAlert, Long> {
+
+}
+
